7a0ce335e92ec3573faafdbcc7d5c55cbef7ee8c
Nikhilg0208 <nikhilg1098@gmail.com>
2024-07-15 02:58:31 +0530

    this is my first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module, Logger } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
